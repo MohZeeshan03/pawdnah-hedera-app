364f3d51c72216add27a6c02db8b68a157cf22b3
Mohammad Zeeshan <104795669+MohZeeshan03@users.noreply.github.com>
2023-08-02 01:13:29 +0500

    Update networks.js

--- src/config/networks.js
+++ src/config/networks.js
@@ -2,7 +2,7 @@ export const networkConfig = {
   testnet: {
     network: "mainnet",
     jsonRpcUrl: "https://mainnet.hashio.io/api", // check out the readme for alternative RPC Relay urls
-    mirrorNodeUrl: "https://mainnet.mirrornode.hedera.com",
+    mirrorNodeUrl: "https://mainnet-public.mirrornode.hedera.com/",
     chainId: "0x127",
   }
 }
